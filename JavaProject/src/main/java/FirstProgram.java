171dcbbab35634050304c22c214f332c41918d9c
ssmusuku <saimusuku11@gmail.com>
2024-01-22 19:25:41 -0600

    prime number

--- JavaProject/src/main/java/FirstProgram.java
+++ JavaProject/src/main/java/FirstProgram.java
@@ -1,5 +1,20 @@
 public class FirstProgram {
+
+    public static boolean isPrime(int n){
+        if(n<=1) {
+            return false;
+        }
+        for(int i =2 ; i<= Math.sqrt(n);i++){
+            if(n % i ==0)
+                return false;
+        }
+        return true;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello");
+        if (isPrime(36)) {
+            System.out.println(" is a prime number.");
+        } else {
+            System.out.println(" is not a prime number.");
+        }
     }
 }
