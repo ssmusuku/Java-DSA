690022997fc46840724dd93878e22590bb14ecd5
ssmusuku <saimusuku11@gmail.com>
2024-02-20 09:12:02 -0600

    Updates on code and adding JAR file

--- SrpingBootProject/src/main/java/com/practice/service/MyService.java
+++ SrpingBootProject/src/main/java/com/practice/service/MyService.java
@@ -3,11 +3,10 @@ package com.practice.service;
 import org.springframework.stereotype.Service;
 
 @Service
-public class MyService implements ApplicationService{
+public class MyService implements ApplicationService {
 
-    @Override
-    public String greet() {
-
-        return "Hello";
-    }
+  @Override
+  public String greet() {
+    return "Hello";
+  }
 }
